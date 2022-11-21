9caa7aef5b74b3136977a7f7e15e9c6c424996a6
lnacuna <lucasnahuel.acuna@nttdata.com>
2022-11-21 16:14:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+
+console.clear();
+
+const main = async () => {
+    mostrarMenu();
+    //pausa();
+}
+
+main();
